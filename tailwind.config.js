05c3717abae64bcc78ded024c9725be997b2163e
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-10-08 09:31:36 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          100: '#c542f5',
+          200: '#9e35c4',
+          300: '#6d2487'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
